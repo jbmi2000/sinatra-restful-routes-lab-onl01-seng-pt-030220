f1b6e62840057cdb103591ae628fe7552caaff02
jbmi2000 <jeff.marbury@gmail.com>
2020-05-12 21:53:14 -0400

    Done.

---
+++ app/models/create_recipe_table.rb
@@ -0,0 +1,10 @@
+class CreateRecipeTable < ActiveRecord::Migration[5.0]
+    def change
+        create_table :recipes do |t|
+            t.string :name
+            t.string :ingredients
+            t.integer :cook_time
+        end
+    end
+
+end
